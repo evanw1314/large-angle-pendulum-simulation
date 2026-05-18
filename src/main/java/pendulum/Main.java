2366d78c0a0e757822bbaf8c353185b637e9a6a0
Evan Wescott <evo.wescott@gmail.com>
2026-05-17 22:20:31 -0700

    Removed some unneccesary variables and comments

--- src/main/java/pendulum/Main.java
+++ src/main/java/pendulum/Main.java
@@ -10,16 +10,7 @@ public class Main {
         Pendulum pendulum = new Pendulum(length, initialAngle);
         RK4Integrator integrator = new RK4Integrator();
 
-        double timeStep = 0.01;
-        double totalTime = 10.0;
-
         new SimulationFrame(pendulum, integrator);
-
-        // for (double t = 0; t <= totalTime; t += timeStep) {
-        //     double[] result = integrator.step(pendulum, timeStep);
-        //     System.out.println("Time: " + t + "s, Angle: " + Math.toDegrees(result[0]) + " degrees, Angular Velocity: " + Math.toDegrees(result[1]) + " degrees/s");
-        // }
-
     }
 
 }
